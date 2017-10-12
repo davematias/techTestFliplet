90c63df8287e08247bbf976f7410c39ee798a553
David Gil Machado Matias <david.matias@aitec.pt>
2017-10-12 21:29:38 +0100

    added the frontend implementation

--- Backend/RssServer/public/javascripts/rssLoader.js
+++ Backend/RssServer/public/javascripts/rssLoader.js
@@ -4,26 +4,51 @@ function RssLoader() {
     this.backendUrl = "http://localhost:3000/showRss?rss_url=";
 }
 
+//loads the rss and prints it to the console
 RssLoader.prototype.printRssToConsole = function(url){
+
+    this.loadRss(url, function(error, data){
+
+        if(error)
+            console.log(error);
+        else
+        {
+            for (var i in data) {
+
+                var item = data[i];
+
+                console.log(item.link);
+                console.log(item.title);
+                console.log(item.description);
+
+                console.log("--------------");
+
+            }
+        }
+
+    });
+
+}
+
+//loads the rss with ajax and returns the data or error
+RssLoader.prototype.loadRss = function(url, callback){
 
     $.ajax({
         url: this.backendUrl + url,
         method: 'GET',
         dataType: 'json'
     }).done(function (response) {
-        if (response.status != 'ok') { throw response.message; }
-    
-        for (var i in response.items) {
-          
-            var item = response.items[i];
-           
-            console.log(item.link);
-            console.log(item.title);
-            console.log(item.description);
 
-            console.log("--------------");
-
+        if (response.status != 'ok')
+            callback(response.message); 
+        else
+        {
+            callback(null, response.items);
         }
+
+    })
+    .fail(function (jqXHR, textStatus) {
+        callback(textStatus); 
     });
 
 }
--- Backend/RssServer/public/javascripts/site.js
+++ Backend/RssServer/public/javascripts/site.js
@@ -6,7 +6,11 @@ $(document).ready(function () {
     var loader = new RssLoader();
 
     loader.printRssToConsole("http://fliplet.com/feed");
-    loader.printRssToConsole("http://feeds.feedburner.com/TechCrunch/ ");
+    loader.printRssToConsole("http://feeds.feedburner.com/TechCrunch/");
     loader.printRssToConsole("http://feeds.bbci.co.uk/news/rss.xml");
+
+    // use the binder to add function to the controls
+    var binder = new Binder();
+    binder.bind();
 
 });
